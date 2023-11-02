d3ae8e4abffdd0dd1196ee5a682c7177740a1aa2
Alexandra-Dunaf <74108083+Alexandra-Dunaf@users.noreply.github.com>
2023-11-02 22:31:44 +0400

    Delete first-hibernate directory

--- first-hibernate/src/main/java/org/example/dao/interfaces/objects/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.interfaces.objects;
-import org.example.dao.interfaces.CommonDAO;
-import org.example.entity.Category;
-import org.example.dao.interfaces.FindDAO;
-
-
-
-public interface CategoryDAO extends CommonDAO<Category>, FindDAO<Category>{
-}
--- first-hibernate/src/main/java/org/example/dao/interfaces/objects/PriorityDAO.java
+++
@@ -1,9 +0,0 @@
-package org.example.dao.interfaces.objects;
-
-
-import org.example.dao.interfaces.CommonDAO;
-import org.example.entity.Priority;
-import org.example.dao.interfaces.FindDAO;
-
-public interface PriorityDAO extends CommonDAO<Priority>, FindDAO<Priority> {
-}
